2b50fe2cc05e9b5faf37ff3c36cb82763f155bfc
yessen <yessen@mail.ru>
2019-01-28 22:34:17 +0600

    added an iterator class

---
+++ lec2/myiterator.py
@@ -0,0 +1,30 @@
+class MyIterator():
+  """ MyIterator class shows how to implement 
+      a basic iterator containing a list of numbers
+      Attributes:
+        L - list of numbers
+        idx - pointer to the current position in the list
+  """
+  L = [1,2,3,4,5,6]
+  idx = 0
+
+  def __iter__(self):
+    """ Returns itself """
+    return self
+ 
+  def __next__(self):
+    """ Return the next element in the list  """
+    if self.idx < len(self.L):
+      item = self.L[self.idx]
+      self.idx += 1
+      return item
+    else:
+      raise StopIteration
+
+if __name__ == "__main__":
+  # create an object of MyIterator class
+  p = MyIterator()
+
+  # traverse a list and print its content
+  for item in p:
+    print(item)
